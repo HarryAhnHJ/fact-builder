146eb62dbc3d8072704d4ae5d632666e1374c6ff
HarryAhnHJ <hadrago1999@gmail.com>
2026-08-18 11:44:07 -0700

    changed start_app.bat form using node.js

--- start_app.bat
+++ start_app.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 echo FactBuilder running at http://localhost:8123  (Ctrl+C to stop)
 start "" http://localhost:8123
-node server.mjs
+"%~dp0venv\Scripts\python.exe" -m http.server 8123 --bind 127.0.0.1
